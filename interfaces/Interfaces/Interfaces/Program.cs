b0c9fe17c154a01f8c14c4f9ac6566c240c7de98
Peter Howard <peterhoward42@gmail.com>
2014-12-22 11:11:48 +0000

    Interfaces experiment

---
+++ interfaces/Interfaces/Interfaces/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new App().run();
+        }
+    }
+
+    class App
+    {
+        public interface ICanTellTheTime
+        {
+            void soTellTheTimeThen();
+        }
+
+        public void run()
+        {
+            // Have a method that demands an input parameter that satisfies
+            // the 'can tell the time' interface and exercise it.
+
+            ICanTellTheTime myTimeTeller = new FraudulentTimeTeller();
+
+            this.exerciseATimeTeller(myTimeTeller);
+        }
+
+        void exerciseATimeTeller(ICanTellTheTime aTimeTeller)
+        {
+            aTimeTeller.soTellTheTimeThen();
+        }
+    }
+
+    class FraudulentTimeTeller : App.ICanTellTheTime {
+
+        public void soTellTheTimeThen()
+        {
+            Console.WriteLine("The time is 42 (I lied)");
+        }
+    }
+}
